c515dad6d236ab01108bc024eedbedc804eb8329
OscarBarco <125104133+OscarBarco@users.noreply.github.com>
2026-09-04 14:07:37 +0000

    primer inicio

---
+++ python_1/sintaxix_basicas.py
@@ -0,0 +1,11 @@
+# Creacion de variables #
+nombre = "alex"         #Var tipo string#
+documento = 13233       #Var tipo entero#
+direccion = "calle 21"  #var tipo string#
+tiene_deuda= False      #var tipo Bool#
+
+print(nombre)           #mostrar en pantalla#
+
+print(" Concatenacion usando +")
+print( "=" * 30)
+print(" Mi nombre es "+ nombre + " y mi documento es "+ str(documento))
